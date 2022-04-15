933a2dce8fcc1b0356aaeea65b9cb379e64e0753
Tai Pham <tai.pham@gs.liferay.com>
2022-04-15 12:11:53 +0700

    Trim whitespace of name tag for searching

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetTagLocalServiceImpl.java
@@ -122,7 +122,7 @@ public class AssetTagLocalServiceImpl extends AssetTagLocalServiceBaseImpl {
 		List<AssetTag> tags = new ArrayList<>();
 
 		for (String name : names) {
-			AssetTag tag = fetchTag(group.getGroupId(), StringUtil.toLowerCase(name));
+			AssetTag tag = fetchTag(group.getGroupId(), StringUtil.toLowerCase(StringUtil.trim(name)));
 
 			if (tag == null) {
 				ServiceContext serviceContext = new ServiceContext();
